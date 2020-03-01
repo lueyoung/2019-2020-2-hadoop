4f3615455c7d379981b7f40162ca667b14597532
lueyoung <lueyoung7@gmail.com>
2020-03-01 07:30:01 +0000

    update

--- 01-hdfs-mkdir/App.java
+++ 01-hdfs-mkdir/App.java
@@ -1,4 +1,4 @@
-package com.young;
+package com.root;
 
 import java.io.IOException;
 import java.net.URI;
@@ -8,20 +8,13 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws IOException, Exception, URISyntaxException
     {
 	Configuration conf = new Configuration();
-	conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
-	//conf.set("fs.defaultFS","hdfs://hadoop1:9000");
 
 	//1 获取hdfs客户端对象
-	//FileSystem fs = FileSystem.get(conf);
 	FileSystem fs = FileSystem.get(new URI("hdfs://hadoop1:9000"), conf, "root");
 
 	//2 在hdfs上创建路径
@@ -29,6 +22,6 @@ public class App
 
 	//3 关闭资源
 	fs.close();
-        System.out.println( "HDFS opt!" );
+        System.out.println( "HDFS mkdir!" );
     }
 }
